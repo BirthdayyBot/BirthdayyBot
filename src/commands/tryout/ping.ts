7fae99a5f126d2b475c1fb480329fecacf18e32b
nikolaischunk <58025175+nikolaischunk@users.noreply.github.com>
2023-01-14 22:08:33 +0100

    finished implementation of ping command

---
+++ src/commands/tryout/ping.ts
@@ -0,0 +1,26 @@
+import { ApplyOptions } from '@sapphire/decorators';
+import { Command } from '@sapphire/framework';
+
+@ApplyOptions<Command.Options>({
+	description: 'ping pong',
+})
+export class PingCommand extends Command {
+	public override registerApplicationCommands(registry: Command.Registry) {
+		// Register slash command
+		registry.registerChatInputCommand({
+			name: this.name,
+			description: this.description
+		});
+	}
+	// slash command
+	public async chatInputRun(interaction: Command.ChatInputCommandInteraction) {
+		const msg = await interaction.reply({ content: 'Ping?', fetchReply: true });
+		const content = `Pong! Bot Latency ${Math.round(this.container.client.ws.ping)}ms. API Latency ${
+			msg.createdTimestamp - interaction.createdTimestamp
+		}ms.`;
+
+		return await interaction.editReply({
+			content: content
+		});
+	}
+}
