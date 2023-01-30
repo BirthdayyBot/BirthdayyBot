00901132bacb9fb480b2ae454163f088e3b9cd53
nikolaischunk <58025175+nikolaischunk@users.noreply.github.com>
2023-01-30 21:39:50 +0100

    small fixes

--- src/lib/discord/channel.ts
+++ src/lib/discord/channel.ts
@@ -1,7 +1,7 @@
 import { container } from '@sapphire/framework';
 import type { TextBasedChannel } from 'discord.js';
 
-export default async function getTextChannel(channel_id: string): Promise<TextBasedChannel> {
+export async function getTextChannel(channel_id: string): Promise<TextBasedChannel> {
 	const channel = await container.client.channels.fetch(channel_id);
 	if (channel == null || !channel.isTextBased()) {
 		throw new Error('Channel is not text based');
