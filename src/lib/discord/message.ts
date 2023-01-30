2fd47d83580644b78491c62cc4397d045ebdd78c
nikolaischunk <58025175+nikolaischunk@users.noreply.github.com>
2023-01-30 21:39:42 +0100

    Added sendMessage

--- src/lib/discord/message.ts
+++ src/lib/discord/message.ts
@@ -1,7 +1,8 @@
 import { container } from '@sapphire/framework';
 import type { Channel, Message } from 'discord.js';
+import { getTextChannel } from './channel';
 
-export default async function fetchMessage(channel_id: string, message_id: string) {
+export async function fetchMessage(channel_id: string, message_id: string) {
 	const channel: Channel | null = await container.client.channels.fetch(channel_id);
 	if (channel == null || !channel.isTextBased()) {
 		throw new Error('Channel is not text based');
@@ -9,4 +10,9 @@ export default async function fetchMessage(channel_id: string, message_id: strin
 	await channel.messages.fetch(message_id);
 	const message: Message = await channel.messages.fetch(message_id);
 	return message;
+}
+
+export async function sendMessage(channel_id: string, options: { content?: string; embeds: any[]; components?: any[] }) {
+	const channel = await getTextChannel(channel_id);
+	return await channel.send(options);
 }
