36f6375a98fe4c57f2ec18f8a44d620d7a43a0b3
nikolaischunk <58025175+nikolaischunk@users.noreply.github.com>
2023-02-08 23:58:52 +0100

    Add a SingleCommand Template

---
+++ src/lib/template/CommandTemplate.ts
@@ -0,0 +1,37 @@
+import { ApplyOptions } from '@sapphire/decorators';
+import { Command } from '@sapphire/framework';
+import generateEmbed from '../../helpers/generate/embed';
+import { container } from '@sapphire/framework';
+import { getCommandGuilds } from '../../helpers/utils/guilds';
+import thinking from '../discord/thinking';
+import replyToInteraction from '../../helpers/send/response';
+import { TemplateCMD } from '../commands/template';
+
+@ApplyOptions<Command.Options>({
+	name: 'template',
+	description: 'Template Command',
+	enabled: true,
+	runIn: ['GUILD_TEXT'],
+	requiredUserPermissions: ['ViewChannel'],
+	requiredClientPermissions: ['SendMessages']
+})
+export class TemplateCommand extends Command {
+	public constructor(context: Command.Context, options: Command.Options) {
+		super(context, {
+			...options
+		});
+	}
+
+	public override async registerApplicationCommands(registry: Command.Registry) {
+		registry.registerChatInputCommand(await TemplateCMD(), {
+			guildIds: getCommandGuilds('testing')
+		});
+	}
+
+	public override async chatInputRun(interaction: Command.ChatInputCommandInteraction) {
+		container.logger.info('testCommand Command');
+		await thinking(interaction);
+		const embed = await generateEmbed({ title: 'Test', description: 'A Test Command' });
+		return await replyToInteraction(interaction, { embeds: [embed] });
+	}
+}
