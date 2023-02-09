f0e12b67355b45b1cb78b53bc7bb42cd7baef5e3
nikolaischunk <58025175+nikolaischunk@users.noreply.github.com>
2023-02-10 00:43:33 +0100

    Add VoteSite

---
+++ src/lib/db/voteSites.ts
@@ -0,0 +1,11 @@
+export const VoteSites = [
+	{ name: 'top.gg', url: 'https://birthdayy.xyz/topgg/vote' },
+	{
+		name: 'discord-botlist.eu',
+		url: 'https://birthdayy.xyz/discord-botlist/vote'
+	},
+	{
+		name: 'discordlist.gg',
+		url: 'https://birthdayy.xyz/discordlist/vote'
+	}
+];
