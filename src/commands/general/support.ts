fcaca67f42add95921abd2803e8392298fc6248b
nikolaischunk <58025175+nikolaischunk@users.noreply.github.com>
2023-02-10 01:20:35 +0100

    Create Support Command

---
+++ src/commands/general/support.ts
@@ -0,0 +1,46 @@
+import { ApplyOptions } from '@sapphire/decorators';
+import { Command } from '@sapphire/framework';
+import generateEmbed from '../../helpers/generate/embed';
+import { getCommandGuilds } from '../../helpers/utils/guilds';
+import thinking from '../../lib/discord/thinking';
+import replyToInteraction from '../../helpers/send/response';
+import { SupportCMD } from '../../lib/commands';
+import { SupportEmbed } from '../../lib/embeds';
+import { discordButton, docsButton } from '../../lib/components/button';
+
+@ApplyOptions<Command.Options>({
+	name: 'support',
+	description: 'Need help? Join my Support Discord Server!',
+	enabled: true,
+	// runIn: ['GUILD_TEXT', 'DM'], CURRENTYY BROKEN
+	preconditions: [['DMOnly', 'GuildTextOnly'] /* any other preconditions here */],
+	requiredUserPermissions: ['ViewChannel'],
+	requiredClientPermissions: ['SendMessages']
+})
+export class SupportCommand extends Command {
+	public constructor(context: Command.Context, options: Command.Options) {
+		super(context, {
+			...options
+		});
+	}
+
+	public override async registerApplicationCommands(registry: Command.Registry) {
+		registry.registerChatInputCommand(await SupportCMD(), {
+			guildIds: getCommandGuilds('global')
+		});
+	}
+
+	public override async chatInputRun(interaction: Command.ChatInputCommandInteraction) {
+		await thinking(interaction);
+		const embed = await generateEmbed(SupportEmbed);
+		await replyToInteraction(interaction, {
+			embeds: [embed],
+			components: [
+				{
+					type: 1,
+					components: [discordButton, docsButton]
+				}
+			]
+		});
+	}
+}
