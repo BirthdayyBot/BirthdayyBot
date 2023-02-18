f3dee280c5e1b5d3cf07931b47acc83a293b5744
nikolaischunk <58025175+nikolaischunk@users.noreply.github.com>
2023-02-18 21:36:20 +0100

    Implement correct MemberRemove File

--- src/listeners/guildMemberLeave.ts
+++ src/listeners/guildMemberLeave.ts
@@ -1,8 +1,16 @@
-import type { Events } from '@sapphire/framework';
+import { Events } from '@sapphire/framework';
 import { Listener } from '@sapphire/framework';
 import type { GuildMember } from 'discord.js';
 
-export class UserEvent extends Listener<typeof Events.GuildMemberRemove> {
+export class UserEvent extends Listener {
+	public constructor(context: Listener.Context, options: Listener.Options) {
+		super(context, {
+			...options,
+			once: true,
+			event: Events.GuildMemberRemove,
+			enabled: true
+		});
+	}
 	public async run(member: GuildMember) {
 		//TODO: remove from database and update birthday list
 		//Code: https://autocode.com/mp/chillihero/birthday-bot/dev/?filename=functions%2Fevents%2Fdiscord%2Fguild%2Fmember%2Fremove.js
