be608a638e79564c91b0be93a0b9adb61fb4dd1d
nikolaischunk <58025175+nikolaischunk@users.noreply.github.com>
2023-02-18 23:22:46 +0100

    Ported unfinished guildMemberLeave Event

---
+++ src/helpers/send/leaveServerLog.ts
@@ -0,0 +1,43 @@
+import generateEmbed from '../generate/embed';
+import { ALARM, BOT_ID, BOT_SERVER_LOG } from '../provide/environment';
+
+const lib = require('lib')({ token: process.env.STDLIB_SECRET_TOKEN });
+
+export default async function leaveServerLog(guild_id: string) {
+	console.log('Removed from Guild');
+	const guildID = guild_id;
+
+	let server_count = await lib.meiraba.utils['@1.0.1'].discord.bot_server_count({
+		bot_id: `${BOT_ID}`
+	});
+
+	try {
+		let guild = await lib.discord.guilds['@0.2.4'].retrieve({
+			guild_id: `${guild_id}`,
+			with_counts: false
+		});
+		console.log(guild);
+	} catch (e) {
+		console.log('couldnt retrieve infos about left guild :C');
+	}
+	let obj = {
+		title: `${ALARM} ${process.env.BOT_NAME} got removed from a Guild`,
+		description: `I am now in \`${server_count}\` guilds`,
+		fields: [
+			{
+				name: `GuildID`,
+				value: `${guildID}`
+			}
+		]
+	};
+
+	let embed = await generateEmbed(obj);
+
+	await lib.discord.channels['@release'].messages.create({
+		channel_id: BOT_SERVER_LOG,
+		content: '',
+		embeds: [embed]
+	});
+
+	return;
+}
--- src/listeners/guildMemberLeave.ts
+++ src/listeners/guildMemberLeave.ts
@@ -1,7 +1,10 @@
+const lib = require('lib')({ token: process.env.STDLIB_SECRET_TOKEN });
 import { Events } from '@sapphire/framework';
 import { Listener } from '@sapphire/framework';
 import type { GuildMember } from 'discord.js';
-
+import { AUTOCODE_ENV, BIRTHDAYY_ID, BOT_ID } from '../helpers/provide/environment';
+import updateBirthdayOverview from '../helpers/update/overview';
+import leaveServerLog from '../helpers/send/leaveServerLog';
 export class UserEvent extends Listener {
 	public constructor(context: Listener.Context, options: Listener.Options) {
 		super(context, {
@@ -15,5 +18,45 @@ export class UserEvent extends Listener {
 		//TODO: remove from database and update birthday list
 		//Code: https://autocode.com/mp/chillihero/birthday-bot/dev/?filename=functions%2Fevents%2Fdiscord%2Fguild%2Fmember%2Fremove.js
 		console.log('member', member);
+
+		const user_id = member.user.id;
+		const guild_id = member.guild.id;
+
+		if (user_id === BIRTHDAYY_ID) {
+			//todo: add custom bot server check
+			// if (customBotServer.includes(guild_id)) {
+			// 	const serverHasCustomBot = 'Dont delete anything its a custom Bot server';
+			// 	console.warn(serverHasCustomBot);
+			// 	return serverHasCustomBot;
+			// }
+			await removeData(guild_id);
+
+			//todo: add error log if removeGuildRequest fails
+		} else if (user_id === BOT_ID) {
+			// remove all Data if Bot got removed from a server
+			await removeData(guild_id);
+		} else {
+			try {
+				let removeUserRequest = await lib.chillihero['birthday-api']['@release'].birthday.delete({
+					user_id: user_id,
+					guild_id: guild_id
+				});
+				if (removeUserRequest.success) {
+					await updateBirthdayOverview(guild_id);
+				}
+			} catch (e) {
+				console.warn(`Couldn not remove birthday from left user`);
+				console.warn(e);
+			}
+		}
+		async function removeData(guild_id: string) {
+			//left user is Bot and not a custom bot server
+			await leaveServerLog(guild_id);
+			//todo: add error log if removeConfig fails
+
+			let removeGuildRequest = await lib.chillihero['birthday-api'][AUTOCODE_ENV].guild.remove({
+				guild_id: guild_id
+			});
+		}
 	}
 }
