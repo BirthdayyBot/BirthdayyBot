62f0a8b35d29a9c062044fd0e874ac98a9496628
nikolaischunk <nikolai.schunk@gmail.com>
2023-02-24 23:23:43 +0100

    🔥Add Stats Update Support for different Bot Sites

--- src/index.ts
+++ src/index.ts
@@ -35,6 +35,20 @@ container.client = new SapphireClient({
 			return guildLanguage || 'en-US';
 		},
 		defaultMissingKey: 'generic:key_not_found'
+	},
+	botList: {
+		clientId: UserIDEnum.BIRTHDAYY,
+		debug: DEBUG,
+		shard: true,
+		autoPost: {
+			enabled: APP_ENV === 'prd',
+			interval: 10_800_000 //3HR
+		},
+		keys: {
+			topGG: process.env.TOPGG_TOKEN,
+			discordListGG: process.env.DISCORDLIST_TOKEN,
+			discordBotList: process.env.DISCORDBOTLIST_TOKEN
+		}
 	}
 });
 
@@ -56,3 +70,5 @@ const main = async () => {
 
 main();
 import './lib/setup/planetscale';
+import { UserIDEnum } from './lib/enum/UserID.enum';
+import { APP_ENV, DEBUG } from './helpers/provide/environment';
