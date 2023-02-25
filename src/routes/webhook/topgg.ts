3045e6ddd64694211714a72f2d08fddc0560e36d
nikolaischunk <nikolai.schunk@gmail.com>
2023-02-25 01:02:23 +0100

    🔥Create TOPGG Webhook endpoint

---
+++ src/routes/webhook/topgg.ts
@@ -0,0 +1,29 @@
+import { ApplyOptions } from '@sapphire/decorators';
+import { ApiRequest, ApiResponse, methods, Route } from '@sapphire/plugin-api';
+import type { APIWebhookTopGGBody } from '../../lib/model/APIWebhookTopGGBody.model';
+import voteProcess from '../../lib/process/vote';
+
+@ApplyOptions<Route.Options>({ route: `webhook/topgg` })
+export class UserRoute extends Route {
+	public async [methods.POST](request: ApiRequest, response: ApiResponse) {
+		const { authorization } = request.headers;
+		if (!authorization || authorization !== process.env.WEBHOOK_SECRET) {
+			return response.status(401).json({ error: 'Unauthorized' });
+		}
+		console.log(request.body);
+
+		const type = (request.body as APIWebhookTopGGBody).type;
+		const user_id = (request.body as APIWebhookTopGGBody).user;
+		switch (type) {
+			case 'test':
+				console.log('topgg webhook test');
+				break;
+			case 'upvote':
+				await voteProcess('topgg', user_id);
+				break;
+			default:
+				return response.status(400).json({ error: 'Bad Request' });
+		}
+		return response.status(200).json({ message: 'TOPGG VERIFIED' });
+	}
+}
