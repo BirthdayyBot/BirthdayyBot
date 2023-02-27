ce853430d2e542da8eb353c8f1fe71a8e2e30459
nikolaischunk <nikolai.schunk@gmail.com>
2023-02-27 21:49:08 +0100

    🔥Implemented createGuildRequest & enableGuildRequest

--- src/helpers/provide/guild.ts
+++ src/helpers/provide/guild.ts
@@ -1,19 +1,43 @@
-import { API_URL } from './environment';
+import { API_SECRET, API_URL } from './environment';
 import { fetch, FetchResultTypes } from '@sapphire/fetch';
 
-export async function leaveGuild(guild_id: string) {
-	const requestURL = new URL(`${API_URL}/guild/leave`);
+export async function leaveGuildRequest(guild_id: string) {
+	const requestURL = new URL(`${API_URL}guild/leave`);
 	requestURL.searchParams.append('guild_id', guild_id);
 	const leaveGuildRequest = await fetch<FetchResultTypes.JSON>(requestURL, FetchResultTypes.JSON);
 	console.log(leaveGuildRequest);
 }
 
-export async function isGuildDisabled(guild_id: string): Promise<boolean> {
-	const requestURL = new URL(`${API_URL}/guild/retrieve/is-disabled`);
+export async function isGuildDisabledRequest(guild_id: string): Promise<boolean> {
+	const requestURL = new URL(`${API_URL}guild/retrieve/is-disabled`);
 	requestURL.searchParams.append('guild_id', guild_id);
-	const isGuildDisabledRequest = await fetch<FetchResultTypes.JSON>(requestURL, FetchResultTypes.JSON);
-	console.log(isGuildDisabledRequest);
-	const { is_disabled } = isGuildDisabledRequest as unknown as { is_disabled: boolean };
-	console.log('is_disabled', is_disabled);
+	const isGuildDisabled = await fetch<FetchResultTypes.JSON>(requestURL, FetchResultTypes.JSON);
+	console.log(isGuildDisabled);
+	const { is_disabled } = isGuildDisabled as unknown as { is_disabled: boolean };
 	return is_disabled!;
+}
+
+export async function enableGuildRequest(guild_id: string) {
+	const requestURL = new URL(`${API_URL}guild/enable`);
+	requestURL.searchParams.append('guild_id', guild_id);
+	const enableGuildRequest = await fetch<FetchResultTypes.JSON>(
+		requestURL,
+		{ method: 'POST', headers: { Authorization: API_SECRET } },
+		FetchResultTypes.JSON
+	);
+	console.log(enableGuildRequest);
+	return enableGuildRequest;
+}
+
+export async function createGuildRequest(guild_id: string, inviter: string | null) {
+	const requestURL = new URL(`${API_URL}guild/create`);
+	requestURL.searchParams.append('guild_id', guild_id);
+	requestURL.searchParams.append('inviter', `${inviter}`);
+	const createGuildRequest = await fetch<FetchResultTypes.JSON>(
+		requestURL,
+		{ method: 'POST', headers: { Authorization: API_SECRET } },
+		FetchResultTypes.JSON
+	);
+	console.log(createGuildRequest);
+	return createGuildRequest;
 }
