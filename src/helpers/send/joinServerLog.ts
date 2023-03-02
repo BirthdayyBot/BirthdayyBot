4bae4bd9f34793b92a663e53f590e2a26aacdf90
nikolaischunk <nikolai.schunk@gmail.com>
2023-03-02 12:41:19 +0100

    🔥Added new Values to the GuildLog message

--- src/helpers/send/joinServerLog.ts
+++ src/helpers/send/joinServerLog.ts
@@ -1,18 +1,15 @@
-import { DurationFormatter } from '@sapphire/time-utilities';
 import { Guild, time } from 'discord.js';
 import { sendMessage } from '../../lib/discord/message';
 import { BotColorEnum } from '../../lib/enum/BotColor.enum';
 import generateEmbed from '../generate/embed';
-import { BOT_NAME, BOT_SERVER_LOG, FAIL } from '../provide/environment';
+import { BOT_NAME, BOT_SERVER_LOG, SUCCESS } from '../provide/environment';
 import getGuildCount from '../provide/guildCount';
 
-export default async function leaveServerLog(guild: Guild) {
-	console.log('Removed from Guild');
+export default async function joinServerLog(guild: Guild) {
+	console.log('Joined Guild');
 	const server_count = getGuildCount();
 	const { id: guild_id, name, description, memberCount, ownerId, joinedTimestamp: rawJoinedTimestamp } = guild;
-	const joinedAgo = time(Math.floor(rawJoinedTimestamp / 1000), 'f');
-	const joinedDate = time(Math.floor(rawJoinedTimestamp / 1000), 'R');
-	const timeServed = new DurationFormatter().format(Date.now() - rawJoinedTimestamp);
+	const joinedTimestamp = time(Math.floor(rawJoinedTimestamp / 1000), 'f');
 	let fields = [
 		{ name: `GuildName`, value: `${name}` },
 		{
@@ -24,15 +21,14 @@ export default async function leaveServerLog(guild: Guild) {
 	if (description) fields.push({ name: `GuildDescription`, value: `${description}` });
 	if (memberCount) fields.push({ name: `GuildMemberCount`, value: `${memberCount}` });
 	if (ownerId) fields.push({ name: `GuildOwnerID`, value: `${ownerId}` });
-	if (rawJoinedTimestamp) fields.push({ name: `GuildJoinedTimestamp`, value: `${joinedDate}\n${joinedAgo}` });
-	if (timeServed) fields.push({ name: `TimeServed`, value: `${timeServed}` });
+	if (rawJoinedTimestamp) fields.push({ name: `GuildJoinedTimestamp`, value: `${joinedTimestamp}` });
+
 	const embedObj = {
-		title: `${FAIL} ${BOT_NAME} got removed from a Guild`,
+		title: `${SUCCESS} ${BOT_NAME} got added to a Guild`,
 		description: `I am now in \`${server_count}\` guilds`,
 		fields: fields,
-		color: BotColorEnum.BIRTHDAYY_DEV
+		color: BotColorEnum.BIRTHDAYY
 	};
-
 	let embed = await generateEmbed(embedObj);
 	await sendMessage(BOT_SERVER_LOG, { embeds: [embed] });
 
