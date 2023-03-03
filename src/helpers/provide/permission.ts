977597409d75c6bac29599da2cd39cfcd86db020
nikolaischunk <nikolai.schunk@gmail.com>
2023-03-03 19:37:29 +0100

    🛠️ Extended Permission checks

--- src/helpers/provide/permission.ts
+++ src/helpers/provide/permission.ts
@@ -12,3 +12,20 @@ export async function hasChannelPermissions(interaction: Interaction, permission
 	const bot_permissions = target_channel.permissionsFor(bot_user);
 	return bot_permissions.has(permissions);
 }
+
+export async function hasUserGuildPermissions(interaction: Interaction, user_id: string, permissions: PermissionResolvable[]): Promise<boolean> {
+	const user = await interaction.guild!.members.fetch(user_id);
+	return user.permissions.has(permissions);
+}
+
+export async function hasUserChannelPermissions(
+	interaction: Interaction,
+	user_id: string,
+	channel_id: string,
+	permissions: PermissionResolvable[]
+): Promise<boolean> {
+	const user = await interaction.guild!.members.fetch(user_id);
+	const target_channel = (await container.client.channels.fetch(channel_id)) as GuildChannel;
+	const bot_permissions = target_channel.permissionsFor(user);
+	return bot_permissions.has(permissions);
+}
