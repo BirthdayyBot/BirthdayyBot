2f66f760751ed8ee3fc5648a9e31cbf8ca4d4851
nikolaischunk <nikolai.schunk@gmail.com>
2023-03-07 15:57:41 +0100

    🐛 Adjusted Status Route

--- src/routes/status.ts
+++ src/routes/status.ts
@@ -5,10 +5,10 @@ import { ApiRequest, ApiResponse, methods, Route } from '@sapphire/plugin-api';
 export class UserRoute extends Route {
 	public [methods.GET](_request: ApiRequest, response: ApiResponse) {
 		response.statusCode = 200;
-		response.json({ message: 'Landing Page!' });
+		response.json({ message: 'Status Test' });
 	}
 
 	public [methods.POST](_request: ApiRequest, response: ApiResponse) {
-		response.json({ message: 'Landing Page!' });
+		response.json({ message: 'Status Test' });
 	}
 }
