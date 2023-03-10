7c5f84106a2db77f41c75630e4216b97ce644ad4
nikolaischunk <nikolai.schunk@gmail.com>
2023-03-10 15:48:22 +0100

    📃 Adjusted TSDocs

--- src/helpers/utils/findOption.ts
+++ src/helpers/utils/findOption.ts
@@ -2,9 +2,9 @@ import type { Command } from '@sapphire/framework';
 
 /**
 Find an option in an interaction object
-    @param {Command.ChatInputCommandInteraction} interaction - interaction object
-    @param {string} option - option key
-    @param {?any} defaultValue - default value - null by default
+    @param interaction - interaction object
+    @param option - option key
+    @param defaultValue - default value - null by default
     @returns {(string | null)}
     */
 export default function findOption(interaction: Command.ChatInputCommandInteraction, option: string, defaultValue: any = null) {
