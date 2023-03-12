153f5d375989a1d9b681879f2309ff0c20d5f050
Williams Bussat <williams@wbussat.fr>
2023-03-12 20:42:13 +0000

    :recycle: refactor vote field value

--- src/lib/embeds/vote.ts
+++ src/lib/embeds/vote.ts
@@ -1,17 +1,5 @@
 import { ARROW_RIGHT, HEART, PLUS, PREMIUM_URL } from '../../helpers/provide/environment';
-
-// ! Currently not working??
-/*
 import { VoteSites } from '../db/voteSites';
- const VoteFields = (): string => {
-	const sites = VoteSites;
-	let value = '';
-	sites.map((site) => {
-		value += `${ARROW_RIGHT}[${site.name}](${site.url})\n`;
-	});
-	container.logger.info('value', value);
-	return value;
-}; */
 
 export const VoteEmbed = {
     title: `${HEART} Support Birthdayy`,
@@ -19,11 +7,7 @@ export const VoteEmbed = {
     fields: [
         {
             name: 'Vote for Birthdayy',
-            value: `
-            ${ARROW_RIGHT} [top.gg](https://birthdayy.xyz/topgg/vote)
-            ${ARROW_RIGHT} [discord-botlist.eu](https://birthdayy.xyz/discord-botlist/vote)
-            ${ARROW_RIGHT} [discordlist.gg](https://birthdayy.xyz/discordlist/vote)
-            `,
+            value: VoteSites.map(({ name, url }) => `${ARROW_RIGHT} [${name}](${url})\n`).join(''),
             inline: false,
         },
         {
