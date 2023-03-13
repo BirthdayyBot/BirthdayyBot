c8670b799c3092aca5461982b86d46e9be6c7b95
nikolaischunk <nikolai.schunk@gmail.com>
2023-03-14 00:20:21 +0100

    ✨ Added REDIS Values & sorted entries

--- src/lib/types/env.d.ts
+++ src/lib/types/env.d.ts
@@ -1,32 +1,36 @@
 declare global {
 	namespace NodeJS {
 		interface ProcessEnv {
-			NODE_ENV: 'development' | 'production';
-			APP_ENV: 'dev' | 'tst' | 'prd';
-			DEBUG: 'TRUE' | 'FALSE';
-			API_URL: string;
 			API_BASE_URL: string;
-			API_SECRET: string;
 			API_EXTENSION: string;
 			API_PORT: string;
-			DISCORD_TOKEN: string;
-			CLIENT_ID: string;
-			BOT_OWNER: string;
-			BOT_NAME: string;
+			API_SECRET: string;
+			API_URL: string;
+			APP_ENV: 'dev' | 'tst' | 'prd';
+			AUTOCODE_ENV: 'dev' | 'release';
 			BOT_AVATAR: string;
 			BOT_COLOR: string;
-			TOPGG_TOKEN: string;
+			BOT_NAME: string;
+			BOT_OWNER: string;
+			CLIENT_ID: string;
+			CUSTOM_BOT: 'TRUE' | 'FALSE' | 'true' | 'false' | '1' | '0' | 'yes' | 'no' | 'y' | 'n';
+			DB_HOST: string;
+			DB_NAME: string;
+			DB_PASSWORD: string;
+			DB_USERNAME: string;
+			DEBUG: 'TRUE' | 'FALSE';
+			DISCORD_TOKEN: string;
 			DISCORDLIST_TOKEN: string;
 			DISCORDBOTLIST_TOKEN: string;
 			MAX_BIRTHDAYS_PER_SITE: string;
-			WEBHOOK_SECRET: string;
-			DB_NAME: string;
-			DB_USERNAME: string;
-			DB_HOST: string;
-			DB_PASSWORD: string;
+			NODE_ENV: 'development' | 'production';
+			REDIS_DB: string;
+			REDIS_HOST: string;
+			REDIS_PASSWORD: string;
+			REDIS_PORT: string;
 			STDLIB_SECRET_TOKEN: string;
-			AUTOCODE_ENV: 'dev' | 'release';
-			CUSTOM_BOT: 'TRUE' | 'FALSE' | 'true' | 'false' | '1' | '0' | 'yes' | 'no' | 'y' | 'n';
+			TOPGG_TOKEN: string;
+			WEBHOOK_SECRET: string;
 		}
 	}
 }
