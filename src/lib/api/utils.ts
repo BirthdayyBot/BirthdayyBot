a2295b5c3af0ddbf847441ccd605409234c583fc
Williams Bussat <williams@wbussat.fr>
2023-03-15 13:17:57 +0000

    :sparkles: Add params validator

--- src/lib/api/utils.ts
+++ src/lib/api/utils.ts
@@ -9,3 +9,35 @@ export function authenticated(token = API_SECRET) {
         (_req: ApiRequest, res: ApiResponse) => res.unauthorized(),
     );
 }
+
+type ValidQueryParams<T = string | string[] > = Array<keyof T> | undefined;
+
+export function validateParams<QueryParams extends Record<string, string | string[]>>(
+    requiredParams?: ValidQueryParams<QueryParams>,
+) {
+    const paramsToValidate = requiredParams ?? Object.keys({} as QueryParams);
+    const missingParams: string[] = [];
+    const invalidParams: Array<{ key: string, receivedType: unknown, expectedType: unknown }> = [];
+
+    return createFunctionPrecondition(
+        (req: ApiRequest<QueryParams>) => {
+            for (const param of paramsToValidate) {
+                if (!(param in req.query)) {
+                    missingParams.push(param as string);
+                } else if (typeof req.query[param as string] !== typeof {} as QueryParams[typeof param]) {
+                    invalidParams.push({
+                        key: param as string,
+                        receivedType: typeof req.query[param],
+                        expectedType: typeof {} as QueryParams[typeof param],
+                    });
+                }
+            }
+            return missingParams.length === 0 && invalidParams.length === 0;
+        },
+        (_req: ApiRequest<QueryParams>, res: ApiResponse) => res.badRequest({
+            missing: missingParams.length > 0 ? `Missing Parameter(s) - ${missingParams.join(', ')}` : undefined,
+            invalid: invalidParams.length > 0 ? `Invalid Parameter(s) - ${invalidParams.join(', ')}` : undefined,
+        }),
+    );
+}
+
