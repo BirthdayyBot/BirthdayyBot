499ee3fc4a4f9b0c3f01958c6a8ba66737d05282
nikolaischunk <nikolai.schunk@gmail.com>
2023-03-17 14:04:59 +0100

    🐛 Fixed Embed Color not showing

--- src/helpers/generate/embed.ts
+++ src/helpers/generate/embed.ts
@@ -5,25 +5,24 @@ import { BOT_AVATAR, BOT_COLOR, BOT_NAME, IS_CUSTOM_BOT } from '../provide/envir
 /**
  * Creates a RichEmbed object with the given information
  *
- * @param {object} embed_information
- * @property {string} title - The title of the embed
- * @property {string} description - The description of the embed
- * @property {string} [author_name] - The name of the author of the embed
- * @property {string} [author_avatar] - The avatar of the author of the embed
- * @property {string} [thumbnail_url] - The URL of the thumbnail to include in the embed
- * @property {string} [image_url] - The URL of the image to include in the embed
- * @property {Array<APIEmbed>} [fields] - An array of fields to include in the embed
- * @property {string} [color] - The color of the embed
+ * @param embed_information
+ * @property title - The title of the embed
+ * @property description - The description of the embed
+ * @property author_name - The name of the author of the embed
+ * @property author_avatar - The avatar of the author of the embed
+ * @property thumbnail_url - The URL of the thumbnail to include in the embed
+ * @property image_url - The URL of the image to include in the embed
+ * @property fields - An array of fields to include in the embed
+ * @property color - The color of the embed
  *
  * @returns embed - A RichEmbed object with the given information
  */
 
-export default function generateEmbed(embed_information: EmbedInformationModel): APIEmbed {
+export default async function generateEmbed(embed_information: EmbedInformationModel): Promise<APIEmbed> {
 	const { title, description, author_name, author_avatar, thumbnail_url, image_url, fields, color } = embed_information;
-
 	return {
 		title: title ?? '',
-		color: parseInt(color as string) ?? BOT_COLOR,
+		color: parseInt(color as string) || BOT_COLOR,
 		description: description,
 		timestamp: new Date().toISOString(),
 		author: {
