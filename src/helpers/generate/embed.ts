5865946a5361392e12079f5254c7ad56b35dc156
Williams Bussat <williams@wbussat.fr>
2023-03-17 02:44:02 +0100

    🚨 Fix error lint

--- src/helpers/generate/embed.ts
+++ src/helpers/generate/embed.ts
@@ -12,17 +12,17 @@ import { BOT_AVATAR, BOT_COLOR, BOT_NAME, IS_CUSTOM_BOT } from '../provide/envir
  * @property {string} [author_avatar] - The avatar of the author of the embed
  * @property {string} [thumbnail_url] - The URL of the thumbnail to include in the embed
  * @property {string} [image_url] - The URL of the image to include in the embed
- * @property {Array<Object>} [fields] - An array of fields to include in the embed
+ * @property {Array<APIEmbed>} [fields] - An array of fields to include in the embed
  * @property {string} [color] - The color of the embed
  *
  * @returns embed - A RichEmbed object with the given information
  */
 
-export default async function generateEmbed(embed_information: EmbedInformationModel): Promise<APIEmbed> {
+export default function generateEmbed(embed_information: EmbedInformationModel): APIEmbed {
 	const { title, description, author_name, author_avatar, thumbnail_url, image_url, fields, color } = embed_information;
 
 	return {
-		title: title,
+		title: title ?? '',
 		color: parseInt(color as string) ?? BOT_COLOR,
 		description: description,
 		timestamp: new Date().toISOString(),
