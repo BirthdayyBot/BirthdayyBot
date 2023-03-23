b5b85973ea67f3da87280c5e7fea5ab59a6efbc0
Williams Bussat <williams@wbussat.fr>
2023-03-23 14:16:24 +0100

    🐛 DB env values

--- src/lib/types/Augments.d.ts
+++ src/lib/types/Augments.d.ts
@@ -31,6 +31,11 @@ declare global {
 			WEBHOOK_SECRET: string;
 
 			// Database
+			DB_NAME: string;
+			DB_USER: string;
+			DB_PASSWORD: string;
+			DB_HOST: string;
+			DB_PORT: string;
 			DB_URL: string;
 
 			// REDIS
