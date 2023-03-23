d21d5c78a2ee487f1d398c6b8099d5782ae8ae86
Williams Bussat <williams@wbussat.fr>
2023-03-23 03:38:21 +0100

    🐛 Fix types in BirthdayList

--- src/helpers/generate/birthdayList.ts
+++ src/helpers/generate/birthdayList.ts
@@ -5,7 +5,7 @@ import { getBeautifiedDate, numberToMonthname } from '../utils/date';import type
 import { EmbedLimits } from '@sapphire/discord-utilities';
 import { container } from '@sapphire/framework';
 import { GuildIDEnum } from '../../lib/enum/GuildID.enum';
-import type { BirthdayWithUserModel } from '../../lib/db';
+import type { Birthday } from '.prisma/client';
 
 export default async function generateBirthdayList(page_id: number, guild_id: string) {
 	const allBirthdaysByGuild = await container.utilities.birthday.get.BirthdaysByGuildID(guild_id);
@@ -38,9 +38,9 @@ export default async function generateBirthdayList(page_id: number, guild_id: st
  * @returns obj.splitBirthdays - Array of Arrays with birthdays
  */
 function getBirthdaysAsLists(
-	allBirthdays: Array<BirthdayWithUserModel>,
+	allBirthdays: Array<Birthday>,
 	maxBirthdaysPerList: number,
-): { birthdays: Array<Array<BirthdayWithUserModel>>; listAmount: number } {
+): { birthdays: Array<Array<Birthday>>; listAmount: number } {
 	const length = allBirthdays.length;
 	// split birthdays into arrays with max length x entries
 	const splitBirthdays = [];
@@ -55,7 +55,7 @@ function getBirthdaysAsLists(
  * @param birthdays
  * @returns embed - Embed with the given values
  */
-async function createEmbed(guild_id: string, allBirthdays: { monthname: string; birthdays: Array<BirthdayWithUserModel> }[]) {
+async function createEmbed(guild_id: string, allBirthdays: { monthname: string; birthdays: Array<Birthday> }[]) {
 	const guild = await getGuildInformation(guild_id);
 	const embed: CustomEmbedModel = {
 		title: `Birthday List - ${guild?.name ?? 'Unknown Guild'}`,
@@ -162,7 +162,7 @@ function prepareBirthdayList() {
 	const monthArray = [];
 	for (let i = 1; i <= 12; i++) {
 		const monthname = numberToMonthname(i);
-		const emptyArray: BirthdayWithUserModel[] = [];
+		const emptyArray: Birthday[] = [];
 		monthArray.push({ monthname, birthdays: emptyArray });
 	}
 	return monthArray;
@@ -171,7 +171,7 @@ function prepareBirthdayList() {
 /**
  * sort all birthdays to the corresponding month object
  */
-function prepareBirthdays(birthdays: Array<BirthdayWithUserModel>): Array<{ monthname: string; birthdays: Array<BirthdayWithUserModel> }> {
+function prepareBirthdays(birthdays: Array<Birthday>): Array<{ monthname: string; birthdays: Array<Birthday> }> {
 	const list = prepareBirthdayList();
 	birthdays.forEach(function(singleBirthday) {
 		const d = new Date(singleBirthday.birthday);
