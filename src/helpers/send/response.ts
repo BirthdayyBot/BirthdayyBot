f3a0d8e31f72e6795acf976b95f7b51a7880ec84
Williams Bussat <williams@wbussat.fr>
2023-03-26 11:12:39 +0200

    refactor: :recycle: Refactor replyToInteraction

--- src/helpers/send/response.ts
+++ src/helpers/send/response.ts
@@ -1,54 +1,18 @@
-import { Command, container } from '@sapphire/framework';
-import type { ActionRowData, APIEmbed, InteractionReplyOptions, JSONEncodable, MessageActionRowComponentData } from 'discord.js';
-import { DEBUG } from '../provide/environment';
+import type { ChatInputCommandInteraction, ContextMenuCommandInteraction, InteractionReplyOptions, MessagePayload } from 'discord.js';
+
 
 /**
- * Sends a message to a Discord channel with the given content and optional embeds and components
- *
- * @param interaction
- * @param args
- * @property {string} args.content - The content of the message to send
- * @property {JSONEncodable<APIEmbed> | APIEmbed} args.embeds - An array of embed objects to include in the message
- * @property {ActionRowData<MessageActionRowComponentData>} args.components - An array of components to include in the message
- * @property {boolean} args.ephemeral - Whether the message should be an ephemeral message
- * @returns {void}
- *
+ * It replies to an interaction, and if the interaction has already been replied to, it edits the reply
+ * instead
+ * @param {Command.ChatInputCommandInteraction | Command.ContextMenuCommandInteraction} interaction -
+ * The interaction object that was passed to your command handler.
+ * @param {string | MessagePayload | InteractionReplyOptions} options - string | MessagePayload |
+ * InteractionReplyOptions
+ * @returns A promise that resolves to the message that was sent.
  */
 export default async function replyToInteraction(
-	interaction: Command.ChatInputCommandInteraction | Command.ContextMenuCommandInteraction,
-	args: {
-		content?: string;
-		embeds?: (JSONEncodable<APIEmbed> | APIEmbed)[];
-		components?: ActionRowData<MessageActionRowComponentData>[];
-		ephemeral?: boolean;
-	},
-): Promise<void> {
-	const { content, embeds, components, ephemeral } = args;
-
-	// Define the response object, which will be used to send the reply to the interaction
-	const response: InteractionReplyOptions = {
-		content: content ?? '',
-		embeds: embeds,
-		components: components,
-		ephemeral: ephemeral ?? false,
-	};
-
-	// If the interaction has already been replied to, or deferred, edit the reply instead
-	if (interaction?.replied || interaction?.deferred) {
-		interaction.editReply(response);
-		return;
-	}
-
-	// If the interaction has not been replied to or deferred, reply to the interaction
-	try {
-		await interaction.reply(response);
-		container.logger.info(DEBUG ? 'replied' : '');
-		return;
-	} catch (error) {
-		// If the interaction has already been replied to or deferred, edit the reply instead
-		container.logger.warn(error);
-		container.logger.info('editReply');
-		await interaction.editReply(response);
-		return;
-	}
+	interaction: ChatInputCommandInteraction | ContextMenuCommandInteraction,
+	options: string | MessagePayload | InteractionReplyOptions,
+) {
+	return await interaction[(interaction.replied || interaction.deferred) ? 'editReply' : 'reply'](options);
 }
