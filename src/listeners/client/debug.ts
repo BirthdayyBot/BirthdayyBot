188d7a7bcb74834682553002dabda235cc0a5717
Williams Bussat <williams@wbussat.fr>
2023-03-31 11:04:58 +0200

    chore: :bug: Fix heartbeat logs

--- src/listeners/client/debug.ts
+++ src/listeners/client/debug.ts
@@ -5,6 +5,15 @@ import { DEBUG } from '../../helpers/provide/environment';
 @ApplyOptions<Listener.Options>({ event: Events.Debug })
 export class ErrorEvent extends Listener<typeof Events.Debug> {
 	public run(message: string) {
+
+		const logs = [
+			'[HeartbeatTimer] Sending a heartbeat.',
+			'[HeartbeatTimer] Heartbeat acknowledged, latency of ',
+			'Heartbeat acknowledged, latency of',
+		];
+
+		if (logs.some(log => message.includes(log))) return;
+
 		return DEBUG ? this.container.logger.debug(message) : null;
 	}
 }
