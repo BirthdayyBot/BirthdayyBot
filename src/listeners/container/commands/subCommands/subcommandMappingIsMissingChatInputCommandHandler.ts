eab482c95794a151df73b4a1780a45c0a44fd3cb
Williams Bussat <williams@wbussat.fr>
2023-03-31 10:20:48 +0200

    fix: :bug: Fix build error

--- src/listeners/container/commands/subCommands/subcommandMappingIsMissingChatInputCommandHandler.ts
+++ src/listeners/container/commands/subCommands/subcommandMappingIsMissingChatInputCommandHandler.ts
@@ -7,7 +7,7 @@ import type { CacheType, Interaction } from 'discord.js';
 
 @ApplyOptions<Listener.Options>({ event: SubcommandPluginEvents.SubcommandMappingIsMissingChatInputCommandHandler })
 export class ChatInputSubcommandErrorEvent extends Listener<typeof SubcommandPluginEvents.SubcommandMappingIsMissingChatInputCommandHandler> {
-	public run(message: Interaction<CacheType>, subcommand: SubcommandMappingMethod, payload: ChatInputSubcommandAcceptedPayload) {
+	public run(_message: Interaction<CacheType>, subcommand: SubcommandMappingMethod, payload: ChatInputSubcommandAcceptedPayload) {
 		return handleCommandErrorAndSendToUser({
 			error: new Error(`Subcommand mapping is missing chat input command handler for ${subcommand.name}`),
 			interaction: payload.interaction, loggerSeverityLevel: 'error', sentrySeverityLevel: 'error',
