bcae9f17b7b5d3257b2250bbc085ec67df6a7046
Williams Bussat <williams@wbussat.fr>
2023-04-01 15:42:54 +0200

    fix: :bug: Fix once shardReady

--- src/listeners/client/shard/shardReady.ts
+++ src/listeners/client/shard/shardReady.ts
@@ -2,7 +2,7 @@ import { ApplyOptions } from '@sapphire/decorators';
 import { Events, Listener } from '@sapphire/framework';
 
 
-@ApplyOptions<Listener.Options>({ event: Events.ShardReady })
+@ApplyOptions<Listener.Options>({ once:true, event: Events.ShardReady })
 export class UserShardEvent extends Listener<typeof Events.ShardReady> {
 	public run(id: number, _unavailableGuilds: Set<string> | undefined) {
 		if (id === 0) {
