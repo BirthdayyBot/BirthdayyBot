6566316b06e1a7ed6d657de9349a15de02b9ac7d
nikolaischunk <nikolai.schunk@gmail.com>
2023-04-07 17:40:13 +0200

    refactor(bot): 🎨 Improve Error Messages & Send Log Information on Events

--- src/lib/model/index.ts
+++ src/lib/model/index.ts
@@ -2,4 +2,5 @@ export * from './APIWebhookTopGG.model';
 export * from './AutocodeAPIResponseModel.model';
 export * from './CustomEmbed.model';
 export * from './EmbedInformation.model';
+export * from './EventInfo.model';
 export * from './GuildConfig.model';
