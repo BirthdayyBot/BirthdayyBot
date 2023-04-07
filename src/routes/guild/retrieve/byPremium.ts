b4160052a8f46e80bd696c774f9b10fac420a2e6
nikolaischunk <nikolai.schunk@gmail.com>
2023-04-07 18:21:05 +0200

    feat(api): 🎨 add authenticated decorator

--- src/routes/guild/retrieve/byPremium.ts
+++ src/routes/guild/retrieve/byPremium.ts
@@ -1,12 +1,14 @@
 import { ApplyOptions } from '@sapphire/decorators';
-import { ApiRequest, ApiResponse, Route, methods } from '@sapphire/plugin-api';
+import { ApiRequest, ApiResponse, methods, Route } from '@sapphire/plugin-api';
+import { authenticated } from '../../../lib/api/utils';
 import getPremiumGuilds from '../../../lib/premium/getGuilds';
 
 @ApplyOptions<Route.Options>({
 	name: 'byPremium',
 	route: 'guild/retrieve/byPremium',
 })
-export class GuildsPremium extends Route {
+export class PremiumGuilds extends Route {
+	@authenticated()
 	public async [methods.GET](_request: ApiRequest, response: ApiResponse) {
 		const guilds = await getPremiumGuilds();
 
