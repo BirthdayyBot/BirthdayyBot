2320474208d90974a2be0945126750f1d724f1a6
nikolaischunk <nikolai.schunk@gmail.com>
2023-04-08 00:59:00 +0200

    refactor(bot): ♻️ Add `remindMeButtonDisabled`

--- src/lib/components/button/remindMe.ts
+++ src/lib/components/button/remindMe.ts
@@ -1,7 +1,14 @@
-export const remindMeButton = {
+import type { APIButtonComponent } from 'discord.js';
+
+export const remindMeButton: APIButtonComponent = {
 	style: 3,
 	label: '⏰ Remind Me in 12hrs',
 	custom_id: 'remind-me-to-vote',
-	disabled: true,
+	disabled: false,
 	type: 2,
+};
+
+export const remindMeButtonDisabled: APIButtonComponent = {
+	...remindMeButton,
+	disabled: true,
 };
