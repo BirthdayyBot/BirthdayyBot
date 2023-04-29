f05e0cb5def46b69c2a33744f133938acf2a9931
Williams Bussat <williams@wbussat.fr>
2023-04-29 14:46:08 +0200

    fix: ✏️ fix typos

--- src/commands/general/support.ts
+++ src/commands/general/support.ts
@@ -12,7 +12,7 @@ import { SupportEmbed } from '../../lib/embeds';
 	name: 'support',
 	description: 'Need help? Join my Support Discord Server!',
 	enabled: true,
-	// runIn: ['GUILD_TEXT', 'DM'], CURRENTYY BROKEN
+	// runIn: ['GUILD_TEXT', 'DM'], CURRENTLY BROKEN
 	preconditions: [['DMOnly', 'GuildTextOnly'] /* any other preconditions here */],
 	requiredUserPermissions: ['ViewChannel'],
 	requiredClientPermissions: ['SendMessages'],
