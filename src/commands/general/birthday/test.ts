212db05b6db0a8eb03cdd0adc7de687fa600e8b0
nikolaischunk <nikolai.schunk@gmail.com>
2023-05-02 12:07:42 +0200

    fix: Revert `ManageGuilds` change

--- src/commands/general/birthday/test.ts
+++ src/commands/general/birthday/test.ts
@@ -1,7 +1,6 @@
 import { Command, RegisterSubCommand } from '@kaname-png/plugin-subcommands-advanced';
-import { interactionSuccess } from '../../../lib/utils/embed';
-import { RequiresUserPermissions } from '@sapphire/decorators';
 import { reply } from '../../../helpers';
+import { interactionProblem, interactionSuccess } from '../../../lib/utils/embed';
 
 @RegisterSubCommand('birthday', (builder) =>
 	builder
@@ -10,9 +9,15 @@ import { reply } from '../../../helpers';
 		.addUserOption((option) => option.setName('user').setDescription('The user to test the birthday for')),
 )
 export class TestCommand extends Command {
-	@RequiresUserPermissions(['ManageGuild'])
 	public override async chatInputRun(interaction: Command.ChatInputInteraction<'cached'>) {
 		const target = interaction.options.getUser('user') ?? interaction.member.user;
+
+		const { memberPermissions } = interaction;
+
+		if (!memberPermissions.has('ManageGuild')) {
+			return reply(interaction, interactionProblem("You don't have the permission to test the current configs."));
+		}
+
 		await this.container.tasks.run('BirthdayReminderTask', {
 			userId: target.id,
 			guildId: interaction.guildId,
