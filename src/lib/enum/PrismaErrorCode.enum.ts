7630c6fca8846baf8badb5feb60da26ae6792f1e
nikolaischunk <nikolai.schunk@gmail.com>
2023-06-14 00:50:34 +0200

    feat: 📝 Add Enum Documentation

--- src/lib/enum/PrismaErrorCode.enum.ts
+++ src/lib/enum/PrismaErrorCode.enum.ts
@@ -1,3 +1,8 @@
+/**
+ * @name PrismaErrorCodeEnum
+ * @description A collection of error codes
+ * @link https://www.prisma.io/docs/reference/api-reference/error-reference#error-codes
+ */
 export const PrismaErrorCodeEnum = {
 	UNIQUE_CONSTRAINT_FAILED: 'P2002',
 	NOT_FOUND: 'P2025',
