5213e54a3274615d9050cd4810c4082f901cdb16
nikolaischunk <nikolai.schunk@gmail.com>
2023-06-14 01:22:21 +0200

    fix: Set description when no blacklist users are added & add thumbnail

--- src/commands/general/blacklist/list.ts
+++ src/commands/general/blacklist/list.ts
@@ -1,6 +1,7 @@
 import { Command, RegisterSubCommand } from '@kaname-png/plugin-subcommands-advanced';
 import type { Blacklist } from '@prisma/client';
 import { time, userMention } from 'discord.js';
+import { ARROW_RIGHT, IMG_BLOCK } from '../../../helpers';
 import { reply } from '../../../helpers/send/response';
 import thinking from '../../../lib/discord/thinking';
 import { generateDefaultEmbed } from '../../../lib/utils/embed';
@@ -10,7 +11,6 @@ export class ListCommand extends Command {
 	public override async chatInputRun(interaction: Command.ChatInputInteraction<'cached'>) {
 		await thinking(interaction);
 		const blacklistedUsers = await this.container.utilities.blacklist.get.BlacklistByGuildId(interaction.guildId);
-		this.container.logger.info('ListCommand ~ overridechatInputRun ~ blacklistedUsers:', blacklistedUsers[0]);
 		const blackListFormatted = processBlacklistedUsers(blacklistedUsers);
 
 		return reply(interaction, {
@@ -18,11 +18,17 @@ export class ListCommand extends Command {
 				generateDefaultEmbed({
 					title: 'Blacklisted Users',
 					description: blackListFormatted,
+					thumbnail: {
+						url: IMG_BLOCK,
+					},
 				}),
 			],
 		});
 
 		function processBlacklistedUsers(users: Blacklist[]): string {
+			if (users.length === 0) {
+				return `${ARROW_RIGHT}No users on the blacklist.`;
+			}
 			const formattedUsers = users.map((user) => {
 				const { userId, added_at } = user;
 				const formattedDate = time(Math.floor(added_at.getTime() / 1000), 'D');
