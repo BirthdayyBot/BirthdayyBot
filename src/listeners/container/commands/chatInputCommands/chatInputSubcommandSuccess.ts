74801618898c844c089e4ffc5529d33a8a7d496f
Williams Bussat <williams@wbussat.fr>
2023-08-05 16:58:14 +0200

    chore: 🚚 rename ChatInputSubcommandSuccess

--- src/listeners/container/commands/chatInputCommands/chatInputSubcommandSuccess.ts
+++ src/listeners/container/commands/chatInputCommands/chatInputSubcommandSuccess.ts
@@ -8,7 +8,9 @@ import { Listener, type ChatInputCommand, container } from '@sapphire/framework'
 import { getSuccessLoggerData } from '../../../../helpers';
 
 @ApplyOptions<Listener.Options>({ event: SubcommandPluginEvents.ChatInputSubcommandSuccess })
-export class ChatInputCommandErrorEvent extends Listener<typeof SubcommandPluginEvents.ChatInputSubcommandSuccess> {
+export class ChatInputSubcommandSuccessEvent extends Listener<
+	typeof SubcommandPluginEvents.ChatInputSubcommandSuccess
+> {
 	public run(
 		interaction: ChatInputCommand.Interaction,
 		subcommand: ChatInputCommandSubcommandMappingMethod,
