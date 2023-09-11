59f0a40cfb1e7cf9e1a9b489f0be743941a42cdf
Williams Bussat <williams@wbussat.fr>
2023-09-11 09:03:34 +0200

    chore:  🚚 rename scheduled task folder

--- src/lib/BirthdayyClient.ts
+++ src/lib/BirthdayyClient.ts
@@ -1,23 +1,16 @@
 import { CLIENT_OPTIONS, WEBHOOK_ERROR } from '#root/config';
 import { PrismaClient } from '@prisma/client';
 import { container, SapphireClient } from '@sapphire/framework';
-import { getRootData } from '@sapphire/pieces';
 import { envIsDefined, envParseNumber, envParseString } from '@skyra/env-utilities';
 import { WebhookClient } from 'discord.js';
-import { join } from 'path';
 
 export class BirthdayyClient extends SapphireClient {
 	public constructor() {
 		super(CLIENT_OPTIONS);
 		this.initailizeDB_URL();
-		this.registerPaths();
 
 		container.prisma = new PrismaClient();
 		container.webhook = WEBHOOK_ERROR ? new WebhookClient(WEBHOOK_ERROR) : null;
-	}
-
-	private registerPaths() {
-		this.stores.get('scheduled-tasks').registerPath(join(getRootData().root, 'tasks'));
 	}
 
 	private initailizeDB_URL() {
