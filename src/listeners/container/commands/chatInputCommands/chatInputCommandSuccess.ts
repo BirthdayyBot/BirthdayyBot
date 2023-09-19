680e3e49893033696e93214aa8f55be675cd8608
Williams Bussat <williams@wbussat.fr>
2023-09-19 15:52:56 +0200

    chore: ♻️ switch subcommand plugin

--- src/listeners/container/commands/chatInputCommands/chatInputCommandSuccess.ts
+++ src/listeners/container/commands/chatInputCommands/chatInputCommandSuccess.ts
@@ -1,8 +1,8 @@
 import { logSuccessCommand } from '#utils/utils';
-import { Subcommand } from '@kaname-png/plugin-subcommands-advanced';
 import { ApplyOptions } from '@sapphire/decorators';
 import { Events, Listener, LogLevel, container, type ChatInputCommandSuccessPayload } from '@sapphire/framework';
 import type { Logger } from '@sapphire/plugin-logger';
+import { Subcommand } from '@sapphire/plugin-subcommands';
 
 @ApplyOptions<Listener.Options>({
 	event: Events.ChatInputCommandSuccess,
