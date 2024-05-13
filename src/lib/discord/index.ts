c39517d0c718003b2207046a8b0929a9be77b9e3
Swiizyy <contact@wbussat.fr>
2024-05-13 14:57:26 +0200

    refactor: remove unused discord role and thinking files

--- src/lib/discord/index.ts
+++ src/lib/discord/index.ts
@@ -2,5 +2,3 @@ export * from '#lib/discord/channel';
 export * from '#lib/discord/guild';
 export * from '#lib/discord/GuildMemberFetchQueue';
 export * from '#lib/discord/message';
-export * from '#lib/discord/role';
-export * from '#lib/discord/thinking';
