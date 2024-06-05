814b8ff515f2d767a5de0c0ce7c7a02b5bfedd43
Swiizyy <contact@wbussat.fr>
2024-06-05 12:14:20 +0200

    refactor: 🌐 remove localization from reboot command

--- src/commands/System/Admin/reboot.ts
+++ src/commands/System/Admin/reboot.ts
@@ -2,23 +2,19 @@ import { BirthdayyCommand } from '#lib/structures';
 import { Events, PermissionLevels } from '#lib/types/Enums';
 import { ApplyOptions } from '@sapphire/decorators';
 import { ApplicationCommandRegistry } from '@sapphire/framework';
-import { applyDescriptionLocalizedBuilder, fetchT } from '@sapphire/plugin-i18next';
 import { envParseBoolean, envParseString } from '@skyra/env-utilities';
 
 @ApplyOptions<BirthdayyCommand.Options>({ permissionLevel: PermissionLevels.BotOwner })
 export class UserCommand extends BirthdayyCommand {
 	public override registerApplicationCommands(registry: ApplicationCommandRegistry) {
 		registry.registerChatInputCommand(
-			(builder) =>
-				applyDescriptionLocalizedBuilder(builder, 'commands/system:rebootDescription').setName('reboot'),
+			(builder) => builder.setName(this.name).setDescription('Reboots the bot.').setDMPermission(false),
 			{ guildIds: [envParseString('CLIENT_MAIN_GUILD')] }
 		);
 	}
 
 	public override async chatInputRun(interaction: BirthdayyCommand.Interaction) {
-		const t = await fetchT(interaction);
-		const content = t('commands/system:reboot');
-		await interaction.reply({ content, ephemeral: true });
+		await interaction.reply('🌀 Rebooting...');
 
 		if (envParseBoolean('INFLUX_ENABLED')) {
 			const { client } = this.container;
