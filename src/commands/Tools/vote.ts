4f6a90ecb4a6942bb56ef38266ab3ddc0ecdb6e1
Swiizyy <contact@wbussat.fr>
2024-06-05 12:31:54 +0200

    refactor: improve vote embed

--- src/commands/Tools/vote.ts
+++ src/commands/Tools/vote.ts
@@ -1,28 +1,25 @@
+import { getSupportedUserLanguageT } from '#lib/i18n/translate';
 import { BirthdayyCommand } from '#lib/structures';
-import { Emojis } from '#utils/constants';
+import { ClientColor } from '#utils/constants';
 import { ApplicationCommandRegistry } from '@sapphire/framework';
-import { applyDescriptionLocalizedBuilder, resolveKey } from '@sapphire/plugin-i18next';
-import type { APIEmbed } from 'discord.js';
+import { applyDescriptionLocalizedBuilder } from '@sapphire/plugin-i18next';
+import { EmbedBuilder } from 'discord.js';
 
 export class VoteCommand extends BirthdayyCommand {
 	public override registerApplicationCommands(registry: ApplicationCommandRegistry) {
 		registry.registerChatInputCommand((builder) => {
-			return applyDescriptionLocalizedBuilder(builder, 'commands/vote:voteDescription')
-				.setName('vote')
-				.setDMPermission(true);
+			return applyDescriptionLocalizedBuilder(builder, 'commands/vote:voteDescription').setName('vote');
 		});
 	}
 
 	public override async chatInputRun(interaction: BirthdayyCommand.Interaction) {
-		const embed = (await resolveKey(interaction, 'commands/vote:embed', {
-			returnObjects: true,
-			arrowRight: Emojis.ArrowRight,
-			heart: Emojis.Heart,
-			topgg: 'https://top.gg/bot/270010330782892032/vote',
-			discordlist: 'https://discordlist.com/bot/270010330782892032/vote',
-			'discord-botlist': 'https://discord-botlist.eu/bot/270010330782892032/vote',
-			premium: 'https://premium.birthdayy.xyz'
-		})) as APIEmbed;
+		const t = getSupportedUserLanguageT(interaction);
+		const embed = new EmbedBuilder()
+			.setAuthor({ name: t('commands/vote:embedTitle'), iconURL: interaction.client.user.displayAvatarURL() })
+			.setDescription(t('commands/vote:embedDescription'))
+			.setColor(ClientColor)
+			.setFooter({ text: interaction.client.user.username })
+			.setTimestamp();
 
 		return interaction.reply({ embeds: [embed] });
 	}
