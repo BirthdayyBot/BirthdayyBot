be9853c8ce09d09ae62cb102e597a2aae0b3e4b4
Williams Bussat <williams@wbussat.fr>
2025-05-28 15:07:16 +0200

    fix: use the correct localized key

--- src/lib/util/utils.ts
+++ src/lib/util/utils.ts
@@ -32,9 +32,8 @@ export function sendLoadingMessage<T extends GuildMessage | Message>(
 	message: T,
 	t: TFunction
 ): Promise<typeof message> {
-	const embed = new EmbedBuilder()
-		.setDescription(pickRandom(t('system:loadingMessages', { returnObjects: true })))
-		.setColor(ClientColor);
+	const loadingMessage = t('system:loading', { returnObjects: true }) as string[];
+	const embed = new EmbedBuilder().setDescription(pickRandom(loadingMessage)).setColor(ClientColor);
 	return message.reply({ embeds: [embed] }) as Promise<T>;
 }
 
