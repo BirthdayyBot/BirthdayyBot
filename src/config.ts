4812cb9536f583cfa5a5f756f3a0c729ebecb2c4
Williams Bussat <williams@wbussat.fr>
2025-06-01 17:40:17 +0200

    Revert "feat: Add caching system with Redis and WeakMap support"
    This reverts commit 93e50847dd29eda7c12a0e67c6d58fa30713664b.

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,4 @@
 import { transformOauthGuildsAndUser } from '#lib/api/utils';
-import { redis } from '#lib/setup/redis';
 import { getHandler } from '#root/languages/index';
 import { minutes } from '#utils/common';
 import { Emojis, LanguageFormatters, rootFolder } from '#utils/constants';
@@ -13,7 +12,14 @@ import {
 	type InternationalizationOptions
 } from '@sapphire/plugin-i18next';
 import type { ScheduledTaskHandlerOptions } from '@sapphire/plugin-scheduled-tasks';
-import { envIsDefined, envParseArray, envParseBoolean, envParseInteger, envParseString } from '@skyra/env-utilities';
+import {
+	envIsDefined,
+	envParseArray,
+	envParseBoolean,
+	envParseInteger,
+	envParseNumber,
+	envParseString
+} from '@skyra/env-utilities';
 import {
 	ActivityType,
 	GatewayIntentBits,
@@ -201,8 +207,16 @@ function parseInternationalizationOptions(): InternationalizationOptions {
 }
 
 function parseBullOptions(): ScheduledTaskHandlerOptions['bull'] {
+	const { REDIS_USERNAME, REDIS_PASSWORD } = process.env;
+
 	return {
-		connection: redis
+		connection: {
+			port: envParseNumber('REDIS_PORT', 6379),
+			password: REDIS_PASSWORD,
+			host: envParseString('REDIS_HOST', 'localhost'),
+			db: envParseInteger('REDIS_DB'),
+			username: REDIS_USERNAME
+		}
 	};
 }
 
--- src/lib/core/domain/cache.ts
+++
@@ -1,9 +0,0 @@
-import type { Awaitable } from '@sapphire/utilities';
-
-export abstract class Cache {
-	public abstract set(key: string, value: unknown, ttl?: number): Awaitable<void>;
-	public abstract get<T>(key: string): Awaitable<T | null>;
-	public abstract delete(key: string): Awaitable<void>;
-	public abstract clear(): Awaitable<void>;
-	public abstract has(key: string): Awaitable<boolean>;
-}
