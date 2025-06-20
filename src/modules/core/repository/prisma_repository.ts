303664998cbac9a61626ef825692d025c8fa36b5
Williams Bussat <williams@wbussat.fr>
2025-06-20 14:35:26 +0200

    feat(repository): enhance repository structure with caching and modular paths

--- src/modules/core/repository/prisma_repository.ts
+++
@@ -1,8 +0,0 @@
-import type { PrismaClient } from '@prisma/client';
-import { BaseRepository } from './base_repository.js';
-
-export abstract class PrismaRepository<T, K> extends BaseRepository<T, K> {
-	public constructor(public readonly db: PrismaClient) {
-		super();
-	}
-}
--- src/modules/user/repository/user_repository.ts
+++ src/modules/user/repository/user_repository.ts
@@ -1,111 +1,50 @@
-import { bento } from '#root/modules/core/services/cache';
-import { prisma } from '#root/modules/core/services/prisma.js';
+import { BaseRepository } from '#core/repository/base_repository';
+import { prisma } from '#core/services/prisma';
+import { User } from '#user/domain/user';
+import { UserIdentifier } from '#user/domain/user_identifier';
 import type { User as PrismaUser } from '@prisma/client';
-import { User } from '../domain/user.js';
-import { UserIdentifier } from '../domain/user_identifier.js';
 
-interface StoreUserDTO {
-	id: string;
-	username: string | null;
-	discriminator: string | null;
-	premium: boolean;
-}
-interface UpdateUserDTO {
-	id: string;
-	username?: string | null;
-	discriminator?: string | null;
-	premium?: boolean;
-}
-
-export class UserRepository {
-	public async findById(id: UserIdentifier): Promise<User | null> {
-		const userRecord = await bento.getOrSet({
-			key: this.getCacheKey(id),
-			factory: () => {
-				return prisma.user.findUnique({
-					where: { id: id.toString() }
-				});
-			},
-			ttl: 60 * 60 // Cache for 1 hour
-		});
-
-		if (!userRecord) {
-			return null;
-		}
-
-		return this.toDomain(userRecord);
+export class UserRepository extends BaseRepository<UserIdentifier, User, PrismaUser> {
+	public constructor() {
+		super('UserRepository', 'multitier');
 	}
 
-	public async create(dto: StoreUserDTO): Promise<User> {
-		const userRecord = await prisma.user.create({
+	public override toDomain(entity: PrismaUser): User {
+		return User.create({
+			id: UserIdentifier.fromString(entity.id),
+			username: entity.username,
+			discriminator: entity.discriminator,
+			createdAt: entity.createdAt,
+			updatedAt: entity.updatedAt,
+			premium: entity.premium
+		});
+	}
+
+	protected createInSource(key: UserIdentifier, entity: Omit<User['props'], 'id'>): Promise<PrismaUser> {
+		return prisma.user.create({
 			data: {
-				id: dto.id,
-				username: dto.username,
-				discriminator: dto.discriminator,
-				premium: dto.premium
+				id: key.toString(),
+				...entity
 			}
 		});
+	}
 
-		await bento.set({
-			key: this.getCacheKey(UserIdentifier.fromString(userRecord.id)),
-			value: userRecord,
-			ttl: 60 * 60
+	protected findFromSource(key: UserIdentifier): Promise<PrismaUser | null> {
+		return prisma.user.findUnique({
+			where: { id: key.toString() }
 		});
-
-		return this.toDomain(userRecord);
 	}
 
-	public async update(dto: UpdateUserDTO): Promise<User | null> {
-		try {
-			const userRecord = await prisma.user.update({
-				where: { id: dto.id },
-				data: {
-					username: dto.username,
-					discriminator: dto.discriminator,
-					premium: dto.premium
-				}
-			});
-
-			await bento.set({
-				key: this.getCacheKey(UserIdentifier.fromString(userRecord.id)),
-				value: userRecord,
-				ttl: 60 * 60
-			});
-
-			return this.toDomain(userRecord);
-		} catch (error) {
-			return null;
-		}
+	protected updateInSource(key: UserIdentifier, entity: Partial<Omit<User['props'], 'id'>>): Promise<PrismaUser> {
+		return prisma.user.update({
+			where: { id: key.toString() },
+			data: entity
+		});
 	}
 
-	public async delete(id: UserIdentifier): Promise<User | null> {
-		const userRecord = await prisma.user
-			.delete({
-				where: { id: id.toString() }
-			})
-			.catch(() => null);
-
-		if (!userRecord) {
-			return null;
-		}
-
-		await bento.delete({ key: this.getCacheKey(id) });
-
-		return this.toDomain(userRecord);
-	}
-
-	private getCacheKey(id: UserIdentifier): string {
-		return `user:${id.toString()}`;
-	}
-
-	private toDomain(userRecord: PrismaUser): User {
-		return User.create({
-			id: UserIdentifier.fromString(userRecord.id),
-			username: userRecord.username,
-			discriminator: userRecord.discriminator,
-			premium: userRecord.premium,
-			createdAt: userRecord.createdAt,
-			updatedAt: userRecord.updatedAt
+	protected deleteFromSource(key: UserIdentifier): Promise<PrismaUser | null> {
+		return prisma.user.delete({
+			where: { id: key.toString() }
 		});
 	}
 }
