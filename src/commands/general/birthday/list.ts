eabdff44672b96e506a533ee1b0fba6662fcdf73
Williams Bussat <williams@wbussat.fr>
2026-01-12 11:40:37 +0100

    fix: remove unnecessary thinking function call in birthday list command

--- src/commands/general/birthday/list.ts
+++ src/commands/general/birthday/list.ts
@@ -1,14 +1,11 @@
 import { Command, RegisterSubCommand } from '@kaname-png/plugin-subcommands-advanced';
 import { generateBirthdayList } from '../../../helpers/';
-import thinking from '../../../lib/discord/thinking';
 
 @RegisterSubCommand('birthday', (builder) =>
 	builder.setName('list').setDescription('List all Birthdays in this Discord server'),
 )
 export class ListCommand extends Command {
 	public override async chatInputRun(interaction: Command.ChatInputInteraction<'cached'>) {
-		await thinking(interaction);
-
 		const paginatedMessage = await generateBirthdayList(interaction.guild);
 
 		await paginatedMessage.run(interaction);
