39211f0aae4e436874c0a9c33854a55087ab0216
Williams Bussat <williams@wbussat.fr>
2026-01-12 11:45:39 +0100

    fix: enhance error handling by ignoring specific Discord API errors in Sentry reporting

--- src/listeners/tasks/scheduledTaskError.ts
+++ src/listeners/tasks/scheduledTaskError.ts
@@ -2,13 +2,14 @@ import { ApplyOptions } from '@sapphire/decorators';
 import { Listener } from '@sapphire/framework';
 import { ScheduledTaskEvents } from '@sapphire/plugin-scheduled-tasks';
 import * as Sentry from '@sentry/node';
-import { logErrorToContainer } from '../../lib/utils/errorHandling';
+import { isExpectedDiscordError, logErrorToContainer } from '../../lib/utils/errorHandling';
 import { envIsDefined } from '@skyra/env-utilities';
 
 @ApplyOptions<Listener.Options>({ event: ScheduledTaskEvents.ScheduledTaskError })
 export class ScheduledTaskErrorEvent extends Listener<typeof ScheduledTaskEvents.ScheduledTaskError> {
 	public run(error: Error, task: string, _payload: unknown) {
-		if (envIsDefined('SENTRY_DSN')) {
+		// Don't report expected Discord API errors to Sentry
+		if (!isExpectedDiscordError(error) && envIsDefined('SENTRY_DSN')) {
 			Sentry.withScope((scope) => {
 				scope.setLevel('error');
 				scope.setTags({ task });
